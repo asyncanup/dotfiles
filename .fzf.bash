abccfeffadcfb9ffe1cb52ed963ba8ef192c59d5
Anup Bishnoi <pixelsallover@gmail.com>
2020-02-18 06:04:29 +0000

    added more to bootstrap, next up is nnn

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/dev/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/dev/.fzf/bin"
+if [[ ! "$PATH" == *~/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/dev/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "~/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/dev/.fzf/shell/key-bindings.bash"
+source "~/.fzf/shell/key-bindings.bash"
