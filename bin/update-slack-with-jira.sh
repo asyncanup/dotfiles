344844e20446980843de8ee191a5c6e07747bfef
Anup Bishnoi <pixelsallover@gmail.com>
2020-05-22 10:56:38 -0700

    [update-slack-with-jira] bikeshed status text

--- bin/update-slack-with-jira.sh
+++ bin/update-slack-with-jira.sh
@@ -6,6 +6,6 @@ info="$(ypx jira-cl:jira issue | grep In.Progress | cut -d ' ' -f3)"
 curl \
   -H 'Content-type: application/json; charset=utf-8' \
   -H "Authorization: Bearer $SLACK_TOKEN" \
-  -d '{"profile":{"status_text":"Working on :jira: '"$info"'"}}' \
+  -d '{"profile":{"status_text":"Want reviews on :jira: '"$info"'"}}' \
   https://slack.com/api/users.profile.set
 
